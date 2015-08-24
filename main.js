cb72b593b07ae1768c6821b5afbd7f386dda8944
Sally Kong <kongsally94@gmail.com>
2015-08-24 09:26:42 -0400

    loading from local json file

--- main.js
+++ main.js
@@ -15,39 +15,38 @@ function initialize() {
 	Parse.initialize("aNDAZRLAojJH7aCQ0my1U0AxUBLVBlba6XhRlGa2", 
 		"pcAOB3TF0npsLLUxM4BWr6gpt5cbYKGUoEqi2pls");
 
-	var HaikuObject = Parse.Object.extend("Haiku");
-	var query = new Parse.Query(HaikuObject);
-	query.find({
-		success : function(results) {
-			haikus = results;
-			populateHaikus();
-			printAnalysis();
-		}
-	});
-
-	// $.getJSON("Haikus.json", function(json) {
-	// 	console.log(json.results);
-	// 	haikus = json.results;
-	// 	populateHaikus();
-	// 	printAnalysis();
+	// var HaikuObject = Parse.Object.extend("Haiku");
+	// var query = new Parse.Query(HaikuObject);
+	// query.find({
+	// 	success : function(results) {
+	// 		haikus = results;
+	// 		populateHaikus();
+	// 		printAnalysis();
+	// 	}
 	// });
+
+	$.getJSON("Haikus.json", function(json) {
+		haikus = json.results;
+		populateHaikus();
+		printAnalysis();
+	});
 }
 
 function populateHaikus() {
 	for(var i = 0; i < haikus.length; i++) {
 		$(".haiku-grid").append("<div class='mdl-cell mdl-card mdl-shadow--2dp'><div class='mdl-card__title mdl-card--expand'><h3 class='mdl-card__title-text'>" 
-			+ haikus[i].get("haiku1").join(' ') + "<br>"
-	    	+ haikus[i].get("haiku2").join(' ') + "<br>"
-	    	+ haikus[i].get("haiku3").join(' ') + "<br>"
+			+ haikus[i].haiku1.join(' ') + "<br>"
+	    	+ haikus[i].haiku2.join(' ') + "<br>"
+	    	+ haikus[i].haiku3.join(' ') + "<br>"
 			+ "</h3></div><div class='mdl-card__supporting-text'>"
-	    	+ "Sally & " + haikus[i].get("writers").join(' &')
-	        + " @ " + haikus[i].get("place")
+	    	+ "Sally & " + haikus[i].writers.join(' &')
+	        + " @ " + haikus[i].place
 	    	+ "</div><div class='mdl-card__actions mdl-card--border'>"
-	        + haikus[i].get("date") 
+	        + haikus[i].date
 	    	+ "</div></div>");
 		countWords(haikus[i]);
 		countFriends(haikus[i]);
-		totalWordCnt += haikus[i].get("wordCount");
+		totalWordCnt += haikus[i].wordCount;
 	}
 	wordObjects.sort(compareCnt);
 	friends.sort(compareCnt);
@@ -82,7 +81,7 @@ function findFriend(name) {
 }
 
 function countFriends(haiku) {
-	var writers = haiku.get("writers");
+	var writers = haiku.writers;
 	for (var i = 0; i < writers.length; i++) {
 		var currentPerson = writers[i].trim();
 		var currentIndex = findFriend(currentPerson);
@@ -95,9 +94,9 @@ function countFriends(haiku) {
 }
 
 function countWords(haiku) {
-	var haiku1 = haiku.get("haiku1");
-	var haiku2 = haiku.get("haiku2");
-	var haiku3 = haiku.get("haiku3");
+	var haiku1 = haiku.haiku1;
+	var haiku2 = haiku.haiku2;
+	var haiku3 = haiku.haiku3;
 
 	for(var i = 0; i < haiku1.length; i++) {
 		var currWord = haiku1[i].toLowerCase();
@@ -153,10 +152,12 @@ function compareCnt(a,b) {
 }
 
 function printAnalysis() {
-	$(".analytics").append("<h4>Total words: " 
-		+ totalWordCnt + "</h4>");
-	$(".analytics").append("<h4>Total unique words: " 
-		+ wordObjects.length + "</h4>");
-	$(".analytics").append("<h4>Total friends: " 
-		+ friends.length + "</h4>");
+	$(".analytics").append("<h5>Total haikus: " 
+		+ haikus.length + "</h5>");
+	$(".analytics").append("<h5>Total words: " 
+		+ totalWordCnt + "</h5>");
+	$(".analytics").append("<h5>Total unique words: " 
+		+ wordObjects.length + "</h5>");
+	$(".analytics").append("<h5>Total friends: " 
+		+ friends.length + "</h5>");
 }
